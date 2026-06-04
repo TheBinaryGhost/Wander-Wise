6c34bc2714e8c0901226612e3af009f40c727c57
The Binary Ghost <themr.shah108@gmail.com>
2026-06-04 13:34:46 +0545

    updated baggage handler

--- handlers/baggage.js
+++ handlers/baggage.js
@@ -6,7 +6,7 @@ const BAGGAGE_ROUTER = Router();
 
 BAGGAGE_ROUTER.post("/", createOrUpdateBaggageValidator, async (req, res, next) => {
     try {
-        const baggage = await create(req.body); // create({ name: "jacket" })
+        const baggage = await create(req.body, req.user); // create({ name: "jacket" })
         res.status(201).json({ data: baggage });
     } catch (error) {
         next(error);
@@ -15,7 +15,7 @@ BAGGAGE_ROUTER.post("/", createOrUpdateBaggageValidator, async (req, res, next) 
 
 BAGGAGE_ROUTER.get("/", async (req, res, next) => {
     try {
-        const baggages = await getAll();
+        const baggages = await getAll(req.user);
         res.status(200).json({ data: baggages });
     } catch (error) {
         next(error);
@@ -24,7 +24,7 @@ BAGGAGE_ROUTER.get("/", async (req, res, next) => {
 
 BAGGAGE_ROUTER.get("/:id", async (req, res, next) => {
     try {
-        const baggage = await getOne(req.params.id);
+        const baggage = await getOne(req.params.id, req.user);
         res.status(200).json({ data: baggage });
     } catch (error) {
         next(error);
@@ -33,7 +33,7 @@ BAGGAGE_ROUTER.get("/:id", async (req, res, next) => {
 
 BAGGAGE_ROUTER.patch("/:id", createOrUpdateBaggageValidator, async (req, res, next) => {
     try {
-        const baggage = await update(req.params.id, req.body);
+        const baggage = await update(req.params.id, req.body, req.user);
         res.status(200).json({ data: baggage });
     } catch (error) {
         next(error);
@@ -42,7 +42,7 @@ BAGGAGE_ROUTER.patch("/:id", createOrUpdateBaggageValidator, async (req, res, ne
 
 BAGGAGE_ROUTER.delete("/:id", async (req, res, next) => {
     try {
-        const baggage = await destroy(req.params.id);
+        const baggage = await destroy(req.params.id, req.user);
         res.status(200).json({ data: baggage });
     } catch (error) {
         next(error);
