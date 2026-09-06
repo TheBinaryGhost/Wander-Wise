e4caac773da2de48c21330d06ae70be9b41ab0cb
The Binary Ghost <themr.shah108@gmail.com>
2026-09-06 17:43:39 +0545

    Update Trip.jsx

--- Frontend/wander-wise/src/pages/trips/Trip.jsx
+++ Frontend/wander-wise/src/pages/trips/Trip.jsx
@@ -78,6 +78,20 @@ const Trip = () => {
             </Link>
           </CardContent>
         </Card>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {data.map((trip) => (
